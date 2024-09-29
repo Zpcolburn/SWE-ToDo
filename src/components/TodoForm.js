f19310c6ddfbca729e6229bd4fc856ef465bcd2b
zach colburn <colburn.zach7@gmail.com>
2024-09-28 19:20:12 -0500

    This is the live demo walkthrough uncomment out all code before demo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,64 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { createTodo, updateTodo } from '../api/todos';
-import { useAuth } from '../utils/context/authContext';
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { Button, Col, Form, Row } from 'react-bootstrap';
+// import { createTodo, updateTodo } from '../api/todos';
+// import { useAuth } from '../utils/context/authContext';
 
-const initialState = {
-  description: '',
-  isComplete: false,
-  createdAt: null,
-};
+// const initialState = {
+//   description: '',
+//   isComplete: false,
+//   createdAt: null,
+// };
 
-export default function TodoForm({ todoObj = initialState, onUpdate }) {
-  const [formInput, setFormInput] = useState(initialState);
-  const { user } = useAuth();
+// export default function TodoForm({ todoObj = initialState, onUpdate }) {
+//   const [formInput, setFormInput] = useState(initialState);
+//   const { user } = useAuth();
 
-  useEffect(() => {
-    setFormInput(initialState);
-  }, [todoObj]);
+//   useEffect(() => {
+//     setFormInput(initialState);
+//   }, [todoObj]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+//   const handleChange = (e) => {
+//     // TODO: <ake a handleChange event.
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const payload = { ...formInput, uid: user.uid, createdAt: new Date() };
-    createTodo(payload).then(({ name }) => {
-      const patchPayload = { firebaseKey: name };
-      updateTodo(patchPayload).then(() => {
-        setFormInput(initialState);
-        onUpdate();
-      });
-    });
-  };
+//   const handleSubmit = (e) => {
+//    // TODO: Make a handleSubmit event.
+//   };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Row className="align-items-center">
-        <Col xs="auto">
-          <Form.Control type="text" placeholder="What's next?" name="description" value={formInput.description} onChange={handleChange} required />
-        </Col>
-        <Col xs="auto">
-          <Button type="submit" variant="primary">
-            Add
-          </Button>
-        </Col>
-      </Row>
-    </Form>
-  );
-}
+//   return (
+//   // TODO: Create a input field and a button to add new items to the list and to submit the item.
+//     <Form onSubmit={}>
 
-TodoForm.propTypes = {
-  todoObj: PropTypes.shape({
-    description: PropTypes.string,
-    isComplete: PropTypes.bool,
-    firebaseKey: PropTypes.string,
-  }),
-  onUpdate: PropTypes.func.isRequired,
-};
+//     </Form>
+//   );
+// }
+
+// TodoForm.propTypes = {
+//   todoObj: PropTypes.shape({
+//     description: PropTypes.string,
+//     isComplete: PropTypes.bool,
+//     firebaseKey: PropTypes.string,
+//   }),
+//   onUpdate: PropTypes.func.isRequired,
+// };
